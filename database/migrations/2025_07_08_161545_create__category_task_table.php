b4dbafa4e85fb548da05a69e4488790b89a469f7
Marwa Emam <marwaemam2411@gmail.com>
2025-07-08 19:34:41 +0200

    fix: updated file content

--- database/migrations/2025_07_08_161545_create__category_task_table.php
+++ database/migrations/2025_07_08_161545_create__category_task_table.php
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('_category_task');
+        Schema::dropIfExists('category_task');
     }
 };
